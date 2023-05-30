2291aa9af42415c35ff746e3b27414fd2f475421
SamScholnickHughes <scholnic@ualberta.ca>
2023-05-30 15:21:56 -0600

    add new main file as main.py and rename old main to old_main.py

--- main.py
+++ main.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from models import HL_model, Regression, Classification
-from experiment.datasets import FGNetDataset
+from new_models import HLGaussian, HLOneBin, Regression
+from experiment.datasets import MegaAgeDataset
 import os
 import sys
 import json
+
 
 def get_model():
     base_model = keras.applications.Xception(
@@ -16,49 +17,36 @@ def get_model():
     )
     return base_model
 
-
 def main(data_file):
-    n_epochs = 50
+    n_epochs = 5
     test_ratio = 0.2
     image_size = 128
     channels = 3
-    batch_size = 32
-    bins = tf.range(-10, 80, dtype=tf.float32)
-    ds = FGNetDataset(data_file, size=image_size, channels=channels)
+    batch_size = 128
+    boarders - tf.linspace(0,100,100)
+    
+    ds = MegaAgeDataset(datafile, size=image_size, channels=channels)
     train, test = ds.get_split(test_ratio)
-
-    input_shape = (image_size, image_size, channels)
+    train = train.batch(batch_size=batch_size)
+    test = test.batch(batch_size=batch_size)
     
-    regress = Regression(get_model(), input_shape)
-    regress_history = regress.train(train, batch_size=batch_size, epochs=n_epochs, save_file="regression_model")
+    
+    hl_gaussian = HLGaussian(get_model(), boarders, 1.0)
+    hl_gaussian_history = hl_gaussian.fit(x=train, epochs=n_epochs, validation_data=test)
+    with open("hl_gaussian_history.json", "w") as file:
+        json.dump(hl_gaussian_history.history, file)
+    
+    hl_one_bin = HLOneBin(get_model(), boarders)
+    hl_one_bin_history = hl_one_bin.fit(x=train, epochs=n_epochs, validation_data=test)
+    with open("hl_one_bin_history.json", "w") as file:
+        json.dump(hl_one_bin_history.history, file)
+    
+    regression = Regression(get_model())
+    regression_history = regression.fit(x=train, epochs=n_epochs, validation_data=test)
     with open("regression_history.json", "w") as file:
-        json.dump(regress_history.history, file)
-    mse = regress.validate(test)
-    with open("regression_mse.txt", mode='w') as file:
-        file.write(f"{mse}\n")
-        
+        json.dump(regression_history.history, file)
     
-    hl_model = HL_model(get_model(), bins, input_shape)
-    hl_history = hl_model.train(train, batch_size=batch_size, epochs=n_epochs, save_file="hl_model")
-    with open("hl_history.json", "w") as file:
-        json.dump(hl_history.history, file)
-    mse = hl_model.validate(test)
-    with open("hl_mse.txt", mode='w') as file:
-        file.write(f"{mse}\n")
-    
-    
-    classification_model = Classification(get_model(), bins, input_shape)
-    classification_history = classification_model.train(train, batch_size=batch_size, epochs=n_epochs, save_file="classification_model")
-    with open("classification_history.json", "w") as file:
-        json.dump(classification_history.history, file)
-    mse = classification_model.validate(test)
-    with open("classification_mse.txt", mode='w') as file:
-        file.write(f"{mse}\n")
-    
-    
-    return
 
-    
 if __name__ == "__main__":
     data_file = sys.argv[1]
     main(data_file)
