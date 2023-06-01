0ac240b3478ae638fb7417bd89696e3dbd056af6
SamScholnickHughes <scholnic@ualberta.ca>
2023-06-01 09:18:09 -0600

    add tuner.py for training hypermodels

--- .ipynb_checkpoints/main-checkpoint.py
+++ .ipynb_checkpoints/main-checkpoint.py
@@ -1,11 +1,12 @@
 import tensorflow as tf
 from tensorflow import keras
 from tensorflow.keras import layers
-from models import HL_model, Regression, Classification
-from experiment.datasets import FGNetDataset
+from new_models import HLGaussian, HLOneBin, Regression
+from experiment.datasets import MegaAgeDataset
 import os
 import sys
 import json
+
 
 def get_model():
     base_model = keras.applications.Xception(
@@ -16,45 +17,36 @@ def get_model():
     )
     return base_model
 
-
 def main(data_file):
-    bins = tf.range(-10, 80, dtype=tf.float32)
-    #path = os.path.join("data", "FGNET", "images")
-    ds = FGNetDataset(data_file, size=128, channels=3)
-    train, test = ds.get_split(0.2)
+    n_epochs = 5
+    test_ratio = 0.2
+    image_size = 128
+    channels = 3
+    batch_size = 128
+    boarders - tf.linspace(0,100,100)
+    
+    ds = MegaAgeDataset(datafile, size=image_size, channels=channels)
+    train, test = ds.get_split(test_ratio)
+    train = train.batch(batch_size=batch_size)
+    test = test.batch(batch_size=batch_size)
+    
+    
+    hl_gaussian = HLGaussian(get_model(), boarders, 1.0)
+    hl_gaussian_history = hl_gaussian.fit(x=train, epochs=n_epochs, validation_data=test)
+    with open("hl_gaussian_history.json", "w") as file:
+        json.dump(hl_gaussian_history.history, file)
+    
+    hl_one_bin = HLOneBin(get_model(), boarders)
+    hl_one_bin_history = hl_one_bin.fit(x=train, epochs=n_epochs, validation_data=test)
+    with open("hl_one_bin_history.json", "w") as file:
+        json.dump(hl_one_bin_history.history, file)
+    
+    regression = Regression(get_model())
+    regression_history = regression.fit(x=train, epochs=n_epochs, validation_data=test)
+    with open("regression_history.json", "w") as file:
+        json.dump(regression_history.history, file)
+    
 
-    base_model = get_model()
-    
-    regress = Regression(base_model, (128,128,3))
-    regress_history = regress.train(train, batch_size=128, epochs=5, save_file="regression_model")
-    with open("regression_history.json", w) as file:
-        json.dump(regress_history.history, file)
-    mse = regress.validate(test)
-    with open("regression_mse.txt", mode='w') as file:
-        file.write(mse)
-        
-    
-    hl_model = HL_model(base_model, bins, (128,128,3))
-    hl_histroy = hl_model.train(train, batch_size=128, epochs=5, save_file="hl_model")
-    with open("hl_history.json", w) as file:
-        json.dump(hl_history.history, file)
-    mse = hl_model.validate(test)
-    with open("hl_mse.txt", mode='w') as file:
-        file.write(mse)
-    
-    
-    classification_model = Classification(base_model, bins, (128,128,3))
-    classification_history = classification_model.train(train, batch_size=128, epochs=5, save_file="classification_model")
-    with open("classification_history.json", w) as file:
-        json.dump(classification_history.history, file)
-    mse = classification_model.validate(test)
-    with open("classification_mse.txt", mode='w') as file:
-        file.write(mse)
-    
-    
-    return
-
-    
 if __name__ == "__main__":
     data_file = sys.argv[1]
     main(data_file)
