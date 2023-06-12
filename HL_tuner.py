792db8058b1aadd48aa5ad4757174cca512f1a22
Kai Luedemann <kluedema@ualberta.ca>
2023-06-12 09:13:09 -0600

    Preparations for HL-Gaussian experiment

--- HL_tuner.py
+++ HL_tuner.py
@@ -19,26 +19,26 @@ def get_model():
     return base_model
 
 def main(base_dir):
-    n_trials = 10
+    seed = 1
+    n_trials = 6
     runs_per_trial = 1
-    n_epochs = 10
-    test_ratio = 0.2
+    n_epochs = 30
     image_size = 128
     channels = 3
     batch_size = 32
-    borders = tf.range(-10,80, 1, tf.float32)
     y_min = 0
     y_max = 70
     directory = os.path.join(base_dir, "hypers")
+
+    keras.utils.set_random_seed(seed)
     
     path = os.path.join(base_dir, "data", "megaage_asian", "megaage_asian")
-    ds = MegaAgeDataset(path, size=image_size, channels=channels)
-    train, test = ds.get_split(test_ratio)
-    train = train.batch(batch_size=batch_size).prefetch(1)
-    test = test.batch(batch_size=batch_size).prefetch(1)
+    ds = MegaAgeDataset(path, size=image_size, channels=channels, batch_size=batch_size)
+    train, test = ds.get_split(None)
     metrics = ["mse", "mae"]
     # tune regression
     hp = kt.HyperParameters()
+    hp.Float("sig_ratio", min_value=0.25, max_value=8., step=2, sampling="log")
     
     hl_gaussian = HyperHLGaussian(get_model, y_min, y_max)
     
@@ -48,14 +48,14 @@ def main(base_dir):
         hypermodel=hl_gaussian, 
         objective = "val_mse", 
         directory=directory, 
-        project_name="hl_gaussian", 
+        project_name="hl_gaussian_sigma", 
         overwrite=False,
-        tune_new_entries=True,
+        tune_new_entries=False,
         max_trials=n_trials, 
-        executions_per_trial=runs_per_trial,
-        distribution_strategy=tf.distribute.MirroredStrategy()
+        executions_per_trial=runs_per_trial
     )
-    hl_gaussian_tuner.search(x=train, epochs=n_epochs, validation_data=test, verbose=2) 
+    callbacks = [keras.callbacks.EarlyStopping(patience=4)]
+    hl_gaussian_tuner.search(x=train, epochs=n_epochs, validation_data=test, verbose=2, callbacks=callbacks) 
 
     data = hl_gaussian_tuner.get_results()
     model = "HL-Gaussian"
--- scripts/hyper_experiment.sh
+++ scripts/hyper_experiment.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
-#SBATCH --job-name=MegaAge-hypers
+#SBATCH --job-name=MegaAge-HLG
 #SBATCH --output=%x-%j.out
-#SBATCH --time=0-12:00:00
+#SBATCH --time=0-9:00:00
 #SBATCH --gres=gpu:1
-#SBATCH --cpus-per-task=3
-#SBATCH --mem=16000M
+#SBATCH --cpus-per-task=2
+#SBATCH --mem=12000M
 #SBATCH --mail-user=kluedema@ualberta.ca
 #SBATCH --mail-type=ALL
 
 DATA=megaage_asian.tar
 HYPERS=hypers.tar
 TUNER=keras_tuner-1.3.5-py3-none-any.whl
+PY_FILE=Histogram_loss/HL_tuner.py
 
 module load python/3.10 scipy-stack cuda cudnn 
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -23,6 +24,6 @@ mkdir $SLURM_TMPDIR/data
 tar xf $DATA -C $SLURM_TMPDIR/data
 tar xf $HYPERS -C $SLURM_TMPDIR
 
-python Histogram_loss/tuner.py $SLURM_TMPDIR
+python $PY_FILE $SLURM_TMPDIR
 
 tar cf $HYPERS -C $SLURM_TMPDIR hypers
