3681db6be3b77be08b5750eb8a4381b22583eee9
Kai Luedemann <kluedema@ualberta.ca>
2023-06-19 14:18:54 -0600

    Prepare reg nostop experiment

--- regression_tuner.py
+++ regression_tuner.py
@@ -11,30 +11,24 @@ from experiment.logging import LogGridSearch
 from experiment.get_model import get_model
 
 
-def main(base_dir, index):
+def main(base_dir):
     keras.utils.set_random_seed(1)
     n_trials = 1
-    runs_per_trial = 1
+    runs_per_trial = 3
     n_epochs = 40
-    test_ratio = 0.2
+    test_ratio = None
     image_size = 128
     channels = 3
     batch_size = 32
-    borders = tf.range(-10,80, 1, tf.float32)
-    y_min = 0
-    y_max = 70
     directory = os.path.join(base_dir, "hypers")
     
     path = os.path.join(base_dir, "data", "megaage_asian", "megaage_asian")
-    ds = MegaAgeDataset(path, size=image_size, channels=channels, aligned=True)
+    ds = MegaAgeDataset(path, size=image_size, channels=channels, batch_size=batch_size, aligned=True)
     train, test = ds.get_split(test_ratio)
-    train = train.batch(batch_size=batch_size).prefetch(1)
-    test = test.batch(batch_size=batch_size).prefetch(1)
     metrics = ["mse", "mae"]
     
-    lrs = [1e-3, 5e-4, 2.5e-4, 1e-4, 5e-5, 2.5e-5, 1e-5]
     hp = kt.HyperParameters()
-    hp.Fixed("learning_rate", lrs[index - 1])
+    hp.Fixed("learning_rate", 1e-4)
     
     regression = HyperRegression(lambda : get_model(model="vgg16"), loss="mse")
     
@@ -44,25 +38,24 @@ def main(base_dir, index):
         hypermodel=regression, 
         objective = "val_mse", 
         directory=directory, 
-        project_name=f"regression_aligned2{index}", 
+        project_name=f"regression_nostop", 
         overwrite=False,
         tune_new_entries=False,
         max_trials=n_trials, 
         executions_per_trial=runs_per_trial,
     ) 
-    callbacks = [keras.callbacks.EarlyStopping(patience=4)]
-    regression_tuner.search(x=train, epochs=n_epochs, validation_data=test, verbose=2, callbacks=callbacks)
+    #callbacks = [keras.callbacks.EarlyStopping(patience=4)]
+    regression_tuner.search(x=train, epochs=n_epochs, validation_data=test, verbose=2)
     
     data = regression_tuner.get_results()
     model = "Regression"
     results = {}
     results[model] = data
 
-    with open(f"regression_aligned2{index}.json", "w") as out_file:
+    with open(f"regression_nostop.json", "w") as out_file:
         json.dump(results, out_file, indent=4)
     
     
 if __name__ == "__main__":
     data_file = sys.argv[1]
-    index = int(sys.argv[2])
-    main(data_file, index)
+    main(data_file)
