20e5b6d14620b873e8123c98b0c53d008635effe
Kai Luedemann <kluedema@ualberta.ca>
2023-06-28 11:50:44 -0600

    Updated dataset class to delay image processing

--- experiment/datasets.py
+++ experiment/datasets.py
@@ -5,89 +5,89 @@ import pandas as pd
 class Dataset:
     """Base dataset class."""
 
-    def __init__(self, buffer_size=100, norm=False, scale=False, batch_size=32, prefetch=1) -> None:
+    def __init__(self, buffer_size=None, batch_size=32, prefetch=1) -> None:
         self.batch_size = batch_size
         self.prefetch = prefetch
-        self.norm = norm
-        self.scale = scale
         self.buf = buffer_size
         self.load()
 
-    def prepare(self, *args):
-        return [x.shuffle(self.buf).batch(self.batch_size).prefetch(self.prefetch) for x in args]
+    def prepare(self, splits):
+        data = []
+        for x in splits:
+            x = self.shuffle(x)
+            x = x.map(self.preprocess, num_parallel_calls=tf.data.AUTOTUNE)
+            x = x.batch(self.batch_size).prefetch(self.prefetch)
+            data.append(x)
+        return data
 
     def load(self):
         pass
 
-    def transform(self, ds):
-        x = ds.map(lambda x, y: x)
-        y = ds.map(lambda x, y : y)
-        if self.norm:
-            x = self.normalize(x)
-        if self.scale:
-            y = self.scale_targets(y)
-        return tf.data.Dataset.zip((x, y))
+    def shuffle(self, data, reshuffle=True):
+        if self.buf is None:
+            buf = len(data)
+        else:
+            buf = self.buf
+        return data.shuffle(buf, reshuffle_each_iteration=reshuffle)
 
-    def normalize(self, ds):
-        x = ds.batch(len(ds)).get_single_element()
-        mu = tf.math.reduce_mean(x, axis=0)
-        std = tf.math.reduce_std(x, axis=0)
-        sig = tf.where(tf.equal(std, 0), tf.ones_like(std), std)
-        x_norm = (x - mu) / sig
-        return tf.data.Dataset.from_tensor_slices(x_norm)
-
-    def scale_targets(self, ds):
-        y = ds.batch(len(ds)).get_single_element()
-        y_max = tf.math.reduce_max(y, axis=0)
-        y_min = tf.math.reduce_min(y, axis=0)
-        y_range = y_max - y_min
-        scale = tf.where(tf.equal(y_range, 0), tf.ones_like(y_range), y_range)
-        y_scaled = (y - y_min) / scale
-        return tf.data.Dataset.from_tensor_slices(y_scaled)
+    def preprocess(self, x):
+        return x
 
     def get_data(self):
         pass
 
-    def __len__(self):
-        return len(self.get_data())
-
-    def input_shape(self):
-        return self.ds.element_spec[0].shape
-
-    def get_split(self, test_ratio):
-        """Return a train-test split for the given test_ratio.
+    def get_split(self, val_ratio, test_ratio=None, shuffle=False):
+        data = self.get_data()
         
-        Params:
-            self - the Dataset object
-            test_ratio - float in range [0,1] indicating the proportion of test samples
+        if shuffle:
+            data = self.shuffle(data, False)
 
-        Returns: a tuple (train, test) of tf.data.Dataset
-        """
+        splits = self.split(data, val_ratio, test_ratio)        
 
-        data = self.transform(self.get_data())
+        return self.prepare(splits)
+    
+    def split(self, data, val_ratio, test_ratio):
+        if test_ratio is not None:
+            return self.three_split(data, val_ratio, test_ratio)
+        else:
+            return self.two_split(data, val_ratio)
+    
+    def two_split(self, data, test_ratio):
+        test_len = self.get_num(len(data), test_ratio)
+        train_len = len(data) - test_len
 
-        train_len = int(len(self) * (1-test_ratio))
-        return self.prepare(data.take(train_len), data.skip(train_len))
+        train = data.take(train_len)
+        test = data.skip(train_len).take(test_len)
+        return train, test
 
-    def three_split(self, val_ratio, test_ratio):
-        data = self.transform(self.get_data())
+    def get_num(self, total, ratio):
+        if ratio >= 1:
+            return ratio
+        else:
+            return int(total * ratio)
 
-        train_len = int(len(self) * (1 - test_ratio - val_ratio))
-        val_len = int(len(self) * val_ratio)
+    def three_split(self, data, val_ratio, test_ratio):
+        test_len = self.get_num(len(data), test_ratio)
+        val_len = self.get_num(len(data), val_ratio)
+        train_len = len(data) - val_len - test_len
+
         train = data.take(train_len)
         val = data.skip(train_len).take(val_len)
         test = data.skip(train_len + val_len)
-        return self.prepare(train, val, test)
+        return train, val, test
+
 
 class CSVDataset(Dataset):
 
-    def __init__(self, path, targets, **kwargs) -> None:
+    def __init__(self, path, targets, drop_cols=[], **kwargs) -> None:
         self.path = path
         self.targets = targets
+        self.drop_cols = drop_cols
         super().__init__(**kwargs)
 
     def load(self):
         df = pd.read_csv(self.path)
+        df = df.drop(self.drop_cols, axis=1)
         x = df.drop(self.targets, axis=1)
         y = df[self.targets]
         ds = tf.data.Dataset.from_tensor_slices((tf.convert_to_tensor(x, dtype=tf.float32),tf.convert_to_tensor(y, dtype=tf.float32)))
@@ -99,7 +99,7 @@ class CSVDataset(Dataset):
 
 class TSDataset(Dataset):
 
-    def __init__(self, path, targets, seq_len, pred_len, drop_cols=[], mode='S', overlap=0, **kwargs) -> None:
+    def __init__(self, path, seq_len, pred_len, targets=None, drop_cols=[], mode='M', overlap=0, **kwargs) -> None:
         self.path = path
         self.targets = targets
         self.seq_len = seq_len
@@ -132,9 +132,6 @@ class TSDataset(Dataset):
 
     def get_data(self):
         return self.ds
-    
-    def __len__(self):
-        return self.n
 
 
 class ImageDataset(Dataset):
@@ -142,7 +139,10 @@ class ImageDataset(Dataset):
     def __init__(self, size=128, channels=3, **kwargs) -> None:
         self.size = size
         self.channels = channels
-        super().__init__(norm=False, **kwargs)
+        super().__init__(**kwargs)
+
+    def preprocess(self, x):
+        return self.parse_image(x)
 
     def parse_image(self, filename):
         label = self.parse_label(filename)
@@ -177,24 +177,29 @@ class MegaAgeDataset(ImageDataset):
         test_ds = tf.data.Dataset.list_files(test_glob, shuffle=False)
 
         y_train, y_test = self.load_labels(len(train_ds), len(test_ds))
+        self.n_train = y_train.shape[0]
+        self.labels = tf.concat((y_train, y_test), axis=0)
+        self.test_dir = dirs[1]
+        self.train = train_ds
+        self.test = test_ds
 
-        self.train = train_ds.map(lambda x : self.parse_image(x, y_train), num_parallel_calls=tf.data.AUTOTUNE)
-        self.test = test_ds.map(lambda x : self.parse_image(x, y_test), num_parallel_calls=tf.data.AUTOTUNE)
-
-    def get_split(self, test_ratio):
-        if test_ratio is None:
-            return self.prepare(self.train, self.test)
+    def split(self, data, val_ratio, test_ratio):
+        if val_ratio is None:
+            return self.train, self.test
         else:
-            return super().get_split(test_ratio)
+            return super().split(data, val_ratio, test_ratio)
         
     def get_data(self):
-        return self.test.concatenate(self.train)
+        return self.train.concatenate(self.test)
 
-    def parse_label(self, filename, labels):
-        file = tf.strings.split(filename, os.sep)[-1]
+    def parse_label(self, filename):
+        path = tf.strings.split(filename, os.sep)
+        
+        img_dir = path[-2]
+        file = path[-1]
         index = tf.strings.to_number(tf.strings.split(file, ".")[0], out_type=tf.int32)
-        label = labels[index - 1]
-        return label
+        index = tf.where(tf.equal(img_dir, "test"), index + self.n_train, index)
+        return self.labels[index - 1]
 
     def load_labels(self, len_train, len_test):
         path = os.path.join(self.path, "list", "train_age.txt")
@@ -205,16 +210,6 @@ class MegaAgeDataset(ImageDataset):
         test_ds = tf.data.TextLineDataset(path).batch(len_test)
         test = tf.strings.to_number(test_ds.get_single_element())
         return train, test
-    
-    def parse_image(self, filename, labels):
-        label = self.parse_label(filename, labels)
-
-        image = tf.io.read_file(filename)
-        image = tf.io.decode_jpeg(image, channels=self.channels)
-        image = tf.image.convert_image_dtype(image, tf.float32)
-        image = tf.image.resize(image, [self.size, self.size])
-        return image, label
-
 
 class FGNetDataset(ImageDataset):
 
@@ -222,7 +217,6 @@ class FGNetDataset(ImageDataset):
         self.path = path
         super().__init__(**kwargs)
         
-
     def parse_label(self, filename):
         parts = tf.strings.split(filename, os.sep)
         label = tf.strings.to_number(tf.strings.substr(parts[-1], 4, 2))
@@ -231,14 +225,14 @@ class FGNetDataset(ImageDataset):
     def load(self):
         glob = os.path.join(self.path, "*")
         list_ds = tf.data.Dataset.list_files(glob, shuffle=False)
-        images_ds = list_ds.map(lambda x : self.parse_image(x))
-        self.data = images_ds
+        self.data = list_ds
     
     def get_data(self):
         return self.data
     
 
 class UTKFaceDataset(ImageDataset):
+
     def __init__(self, path, **kwargs):
         self.path = path
         super().__init__(**kwargs)
@@ -251,9 +245,7 @@ class UTKFaceDataset(ImageDataset):
     def load(self):
         glob = os.path.join(self.path, "*")
         list_ds = tf.data.Dataset.list_files(glob, shuffle=False)
-        list_ds = list_ds.shuffle(len(list_ds), reshuffle_each_iteration=False)
-        images_ds = list_ds.map(lambda x : self.parse_image(x))
-        self.data = images_ds
+        self.data = list_ds
 
     def get_data(self):
         return self.data
