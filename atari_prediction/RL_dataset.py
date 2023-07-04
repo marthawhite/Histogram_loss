d54b9008d800c2a7d92c0fdaed8e46ee86d38705
SamScholnickHughes <scholnic@ualberta.ca>
2023-07-04 12:04:13 -0600

    working RL dataset

--- atari_prediction/RL_dataset.py
+++ atari_prediction/RL_dataset.py
@@ -5,47 +5,46 @@ import os
 
 
 
-def generator(env, actions_file, file_length):
-    f = open(actions_file, "rb")
-    file_length = os.stat(actions_file).st_size
-    for i in range(file_length):
-        byte = f.read(1)
-        val = int.from_bytes(byte, 'big')
-        if(val==82):
-            obs, info = env.reset()
-            #yield np.array(obs)
-        else:
-            obs, r, done, _,_ = env.step(val - 97)
-            yield np.array(obs)
-    
-    
+class Generator:
+    def __init__(self, action_file):
+        game = "Pong" +"NoFrameskip-v4"
+        self.env = gym.make(game)
+        self.action_file = action_file
+        
+    def __call__(self):
+        f = open(self.action_file)
+        file_length = os.stat(self.actions_file).st_size
+        for i in range(file_length):
+            byte = f.read(1)
+            val = int.from_bytes(byte, 'big')
+            if(val==82):
+                obs, info = self.env.reset()
+                yield np.array(obs)
+            else:
+                obs, r, done, _,_ = self.env.step(val - 97)
+                yield np.array(obs)
+
     
     
 
     
     
-def get_dataset():
-    game = "Pong" +"NoFrameskip-v4"
-    env = gym.make(game)
-    action_file_name = "PongNoFrameskip-v4.txt"
-    #returns_file = "returns_file.npy"
+def get_dataset(returns_file):
+    gen = Generator(action_file = "PongNoFrameskip-v4.txt")
     
     inputs = tf.data.Dataset.from_generator(
-        generator,
-        output_signature = tf.TensorSpec(shape=(4, 84, 84), dtype=tf.uint8),
-        args = [env, action_file_name]
+        gen,
+        output_signature = tf.TensorSpec(shape=(4, 84, 84), dtype=tf.uint8)
     )
     
-    #outputs = tf.data.Dataset.from_tensor(tf.convert_to_tensor(np.load(returns_file)))
+    outputs = tf.data.Dataset.from_tensor(tf.convert_to_tensor(np.load(returns_file)))
     
-    #ds = tf.data.Dataset.zip(inouts, ouputs)
-    #ds = ds.shuffle(1000).batch(32).prefetch(1)
-    return inputs
+    ds = tf.data.Dataset.zip(inouts, ouputs)
+    ds = ds.shuffle(1000).batch(32).prefetch(1)
+    return ds
     
 
 
-
-ds = get_dataset()
 
 
 
