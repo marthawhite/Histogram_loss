9f6fcbfd2ad83c176bb68749964498e5c5ef28bf
Kai Luedemann <kluedema@ualberta.ca>
2023-07-04 11:00:17 -0600

    Updated atari models

--- experiment/base_model.py
+++ experiment/base_model.py
@@ -1,12 +1,24 @@
 import tensorflow as tf 
 from tensorflow import keras
-from tensorflow.keras import layers, models
+from keras import layers, models
 
+
+
+def test_model():
+    return models.Sequential([
+        layers.Permute((2, 3, 1)),
+        layers.Conv2D(32, 8, 4, activation="relu"),
+        layers.Conv2D(64, 4, 2, activation="relu"),
+        layers.Conv2D(64, 3, 1, activation="relu"),
+        layers.Flatten(),
+        layers.Dense(512, activation="relu")
+        ])
 
 
 def get_base_model(image_size = (84,84), num_images=4, output_size=1, output_activation=None):
-    inputs = layers.Input(shape=((image_size[0], image_size[1], num_images)))
-    x = layers.Conv2D(filters = 64, kernel_size=(3,3), padding="same", activation="relu")(inputs)
+    inputs = layers.Input(shape=((num_images, image_size[0], image_size[1])))
+    x = layers.Reshape((image_size[0], image_size[1], num_images))(inputs)
+    x = layers.Conv2D(filters = 64, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.BatchNormalization(axis=[1,2,3])(x)
     x = layers.Conv2D(filters = 64, kernel_size=(3,3), padding="same", activation="relu")(x)
     x = layers.BatchNormalization(axis=[1,2,3])(x)
