22526f68e693883d5a9471bc730d890210640c2d
Kai Luedemann <kluedema@ualberta.ca>
2023-07-12 11:49:03 -0600

    Added documentation to atari dataset

--- experiment/atari_dataset.py
+++ experiment/atari_dataset.py
@@ -1,10 +1,24 @@
+"""Class for atari prediction RL datasets."""
+
 import tensorflow as tf
 from experiment.datasets import Dataset
 import numpy as np
 import os
 import gym
 
+
 class RLDataset(Dataset):
+    """A dataset containing observations and returns for an RL agent from an atari game.
+    
+    Params:
+        action_file - path to file containing the agent's actions
+        returns_file - path to file containing the precomputed returns
+        game - the name of the game
+        kwargs - dataset superclass arguments (batch_size, buffer_size, prefetch)
+
+    NOTE: This dataset returns a function that alternates between returning training and testing generators.
+        For correct results, always alternate between training and testing iterations.
+    """
 
     def __init__(self, action_file, returns_file, game=None, **kwargs) -> None:
         super().__init__(**kwargs)
@@ -15,6 +29,13 @@ class RLDataset(Dataset):
         self.env = self.get_env(game)
 
     def get_env(self, game):
+        """Initialize the game environment.
+        
+        Params:
+            game - the name of the game environment
+
+        Returns: the gym environment
+        """
         env = gym.make(game)
         env.seed(1)
         env = gym.wrappers.ResizeObservation(env, (84, 84))
@@ -22,6 +43,13 @@ class RLDataset(Dataset):
         return gym.wrappers.FrameStack(env, 4)
 
     def get_returns(self, returns_file):
+        """Load and scale the returns from a file.
+        
+        Params:
+            returns_file - the path to the .npy file containing the returns
+
+        Returns: the returns scaled to [0, 1]
+        """
         data = np.load(returns_file)
         max_val, min_val = np.max(data), np.min(data)
         scale = max_val - min_val
@@ -30,22 +58,39 @@ class RLDataset(Dataset):
         return (data - min_val) / scale
 
     def train_gen(self, limit):
+        """Generate training samples for a number of runs.
+        
+        Params:
+            limit - the number of game iterations (runs) used for training
+
+        Yields: (obs, return)
+            obs - the (4, 84, 84) image stack as a numpy array
+            return - the scaled return for the corresponding timestep
+        """
         n = -1
         self.reset_file()
         byte = self.file.read(1)
         while True:
             if byte == b'R':
+                # Run finished
                 obs, info = self.env.reset()
                 n += 1
                 if n == limit:
                     return
             else:
+                # Yield observation
                 obs, r, done, _,_ = self.env.step(ord(byte) - 97)
                 self.i += 1
                 yield np.array(obs), self.returns[self.i]
             byte = self.file.read(1)
 
     def test_gen(self):
+        """Generate test samples until the end of the file.
+        
+        Yields: (obs, return)
+            obs - the (4, 84, 84) image stack as a numpy array
+            return - the scaled return for the corresponding timestep
+        """
         byte = self.file.read(1)
         while byte != b"":
             if byte == b'R':
@@ -59,6 +104,11 @@ class RLDataset(Dataset):
     
 
     def gen(self):
+        """Return a generator to generate train or test samples.
+        NOTE: Alternates between producing train and test generators each time it is called.
+
+        Returns: a generator yielding (obs, return) pairs where obs is a (4, 84, 84) numpy image stack
+        """
         if self.train:
             gen = self.train_gen(self.train_n)
         else:
@@ -67,11 +117,21 @@ class RLDataset(Dataset):
         return gen
 
     def reset_file(self):
+        """Reset the actions file at the beginning of an epoch."""
         self.file.seek(0)
         self.env.reset(seed=1)
         self.i = -1
 
     def get_split(self, val_ratio):
+        """Return a dataset that allows train/test split iteration.
+        
+        Params:
+            val_ratio - the proportion of samples to use in the test split
+        
+        Returns: (ds, ds)
+            ds - a generator dataset that alternates between producing train and test samples each time iteration starts
+                NOTE: Always alternate between training and validation when using this dataset!
+        """
         self.train = True
         n = self.file.read().count(b'R')
         self.train_n = int(n * (1 - val_ratio))
