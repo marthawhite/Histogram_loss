d0ae6a3900663e98e92599d40d1330737437dd0e
Kai Luedemann <kluedema@ualberta.ca>
2023-07-31 14:11:14 -0600

    Reverted to standardization for comparable metrics

--- Time_Series/main.py
+++ Time_Series/main.py
@@ -16,11 +16,11 @@ def get_time_series_dataset(filename, drop=[], seq_len=720, train_len=20, pred_l
     df = pd.read_csv(filename)
     df = df.drop(drop, axis = 1)
     df = tf.convert_to_tensor(df, dtype=tf.float32)
-    dmin = tf.reduce_min(df, axis=0)
-    dmax = tf.reduce_max(df, axis=0)
-    dif = dmax - dmin
-    scale = tf.where(dif == 0, 1., dif)
-    df = (df - dmin) / scale
+    mu = tf.reduce_mean(df, axis=0)
+    sig = tf.math.reduce_std(df, axis=0)
+    scale = tf.where(sig == 0, 1., sig)
+    df = (df - mu) / scale
+    
     n = df.shape[0] 
     split = round((1-test_size)*n)
     data = df
@@ -28,6 +28,8 @@ def get_time_series_dataset(filename, drop=[], seq_len=720, train_len=20, pred_l
     test = data[split:]
     inputs = train[:-(train_len)]
     target = train[seq_len:]
+    dmin = tf.reduce_min(train, axis=0)
+    dmax = tf.reduce_max(train, axis=0)
     x_train = keras.utils.timeseries_dataset_from_array(inputs, None, seq_len, batch_size=None)
     y_train = keras.utils.timeseries_dataset_from_array(target, None, train_len, batch_size=None).map(reshape(pred_len, chans))
     ds_train = tf.data.Dataset.zip((x_train, y_train)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
@@ -36,10 +38,10 @@ def get_time_series_dataset(filename, drop=[], seq_len=720, train_len=20, pred_l
     x = keras.utils.timeseries_dataset_from_array(inputs, None, seq_len, batch_size=None)
     y = keras.utils.timeseries_dataset_from_array(targets, None, pred_len, batch_size=None).map(reshape(pred_len, chans))
     ds_test = tf.data.Dataset.zip((x,y)).batch(batch_size).prefetch(tf.data.AUTOTUNE)
-    return ds_train, ds_test
+    return ds_train, ds_test, dmin, dmax
 
 
-def get_bins(n_bins, pad_ratio, sig_ratio):
+def get_bins(n_bins, pad_ratio, sig_ratio, low=0., high=1.):
     """Return the histogram bins given the HL parameters.
     
     Params:
@@ -51,15 +53,15 @@ def get_bins(n_bins, pad_ratio, sig_ratio):
         borders - a Tensor of n_bins + 1 bin borders
         sigma - the sigma to use for HL-Gaussian
     """
-    bin_width = 1 / (n_bins - 2 * sig_ratio * pad_ratio)
+    bin_width = (high - low) / (n_bins - 2 * sig_ratio * pad_ratio)
     pad_width = sig_ratio * pad_ratio * bin_width
-    borders = tf.linspace(-pad_width, 1 + pad_width, n_bins + 1)
+    borders = tf.linspace(low - pad_width, high + pad_width, n_bins + 1)
     sigma = bin_width * sig_ratio
     return borders, sigma
 
 
 def main(data_path):
-    pred_len = 96
+    pred_len = 336
     seq_len = 96
     epochs = 8
     sig_ratio = 2.
@@ -72,15 +74,15 @@ def main(data_path):
     test_ratio = 0.2
     batch_size = 32
     drop = "date"
-    train, test = get_time_series_dataset(data_path, drop, seq_len, pred_len, pred_len, test_ratio, batch_size, chans)
+    train, test, dmin, dmax = get_time_series_dataset(data_path, drop, seq_len, pred_len, pred_len, test_ratio, batch_size, chans)
     metrics = ["mse", "mae"]
     lr = 1e-4
     sched = lambda e, r: lr * 0.5 ** e
     callbacks = [keras.callbacks.EarlyStopping("val_mse", patience=3), keras.callbacks.LearningRateScheduler(sched)]
 
-    borders, sigma = get_bins(n_bins, pad_ratio, sig_ratio)
+    borders, sigma = get_bins(n_bins, pad_ratio, sig_ratio, dmin, dmax)
     borders = tf.expand_dims(borders, -1)
-    borders = tf.expand_dims(borders, -1)
+    sigma = tf.expand_dims(sigma, -1)
 
     shape = train.element_spec[0].shape[1:]
 
