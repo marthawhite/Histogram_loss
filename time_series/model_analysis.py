d1070cbe87d2f3770acd1c734cb7388ead1096d7
SamScholnickHughes <scholnic@ualberta.ca>
2023-08-24 15:57:12 -0600

    add model_analysis.py that saves the test predictions for ETTh2 and ETTm1 for HL and Regression transformer networks

---
+++ time_series/model_analysis.py
@@ -0,0 +1,89 @@
+"""Module containing the run code for time series experiments.
+
+Usage: 
+    python main.py data_path
+
+Params:
+    data_path - path to the input data file
+"""
+
+import tensorflow as tf
+from tensorflow import keras
+from experiment.models import HLGaussian, Regression
+from time_series.base_models import transformer, linear, lstm_encdec
+import json
+from experiment.bins import get_bins
+from time_series.datasets import get_time_series_dataset
+import numpy as np
+
+
+def main():
+    """Run the time series experiment.
+    
+    Params:
+        data_path - path to the data file
+    """
+    datasets = ["ETTh2", "ETTm1"] #["ETTh1", "ETTh2", "ETTm1", "ETTm2"]
+    pred_len = 336
+    seq_len = 336
+    epochs = 100
+    sig_ratio = 2.
+    pad_ratio = 3.
+    n_bins = 100
+    chans = 7
+    head_size = 512
+    n_heads = 8
+    features = 128
+    layers = 2
+    width = 512
+    test_ratio = 0.25
+    batch_size = 32
+    drop = "date"
+    metrics = ["mse", "mae"]
+    lr = 1e-3
+
+    for dataset in datasets:
+        keras.utils.set_random_seed(1)
+        data_path = f"{dataset}.csv"
+        train, test, dmin, dmax = get_time_series_dataset(data_path, drop, seq_len, pred_len, pred_len, test_ratio, batch_size, chans)
+
+        borders, sigma = get_bins(n_bins, pad_ratio, sig_ratio, dmin, dmax)
+        borders = tf.expand_dims(borders, -1)
+        sigma = tf.expand_dims(sigma, -1)
+
+        shape = train.element_spec[0].shape[1:]
+        
+        test_inputs = test.map(lambda x,y: x)
+        test_targets = test.map(lambda x,y: y).unbatch()
+        elements = np.array([element for element in test_targets.as_numpy_iterator()])
+        np.save(f"{dataset}_targets", elements)
+        
+
+        base = transformer(shape, head_size, n_heads, features)
+        # #base = linear(chans, seq_len)
+        # base = lstm_encdec(width, layers, 0.5, shape)
+
+        hlg = HLGaussian(base, borders, sigma, out_shape=(chans, pred_len))    
+        hlg.compile(keras.optimizers.Adam(lr), None, metrics)
+        hist = hlg.fit(train, epochs=epochs, verbose=2, validation_data=test)
+        with open(f"HL_lstm_{dataset}.json", "w") as file:
+            json.dump(hist.history, file)
+        predictions = hlg.predict(test_inputs)
+        np.save(f"{dataset}_HL", predictions)
+        
+
+        base = transformer(shape, head_size, n_heads, features)
+        #base = linear(chans, seq_len)
+        #base = lstm_encdec(width, layers, 0.5, shape)
+
+        reg = Regression(base, out_shape=(pred_len,))    
+        reg.compile(keras.optimizers.Adam(lr), "mse", metrics)
+        hist = reg.fit(train, epochs=epochs, verbose=2, validation_data=test)
+        with open(f"Reg_linear_{dataset}.json", "w") as file:
+            json.dump(hist.history, file)
+        predictions = reg.predict(test_inputs)
+        np.save(f"{dataset}_reg", predictions)
+
+
+if __name__ == "__main__":
+    main()
