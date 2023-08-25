f44b7c81191e6d6cee4f25cfddc20fbaba60c203
Kai Luedemann <kluedema@ualberta.ca>
2023-08-25 16:36:57 -0600

    Modify for Alien

--- atari_prediction/results_vis.py
+++ atari_prediction/results_vis.py
@@ -6,7 +6,7 @@ import matplotlib.pyplot as plt
 
 
 def main(dir_path):
-    epochs = 36
+    epochs = 35
     avgs = []
     preds = []
     for mode in ["test"]:
@@ -18,22 +18,23 @@ def main(dir_path):
             reg = np.load(reg_path)
             y_path = os.path.join(dir_path, f"{mode}.npy")
             y = np.load(y_path)
-            weights_path = os.path.join(dir_path, f"Reg_{i}_w.npy")
+            weights_path = os.path.join(dir_path, f"HL_{i}_w.npy")
             weights = np.load(weights_path, allow_pickle=True)
-            avgs.append([np.mean(x) for x in weights])
-            a = [np.mean(np.abs(x)) for x in weights]
-            print(a)
-            preds.append(np.mean(reg[:300]))
-            print(preds[-1] - np.mean(y[:300]))
-            plt.plot(y)
-            plt.plot(hl)
-            plt.plot(reg)
-            plt.title(f"{mode} {i}")
-            plt.show()
+            avgs.append([np.mean(x) for x in weights[:-2]])
+            print(avgs[-1])
+            # a = [np.mean(np.abs(x)) for x in weights]
+            # print(a)
+            # preds.append(np.mean(reg[:300]))
+            # print(preds[-1] - np.mean(y[:300]))
+            # plt.plot(y)
+            # plt.plot(hl)
+            # plt.plot(reg)
+            # plt.title(f"{mode} {i}")
+            # plt.show()
     avgs = np.stack(avgs)
     for x in avgs.T:
         plt.plot(x)
-    plt.plot(preds)
+    #plt.plot(preds)
     plt.show()
             
 
