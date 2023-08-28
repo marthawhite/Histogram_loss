d9a704d536a5784e668882c662d97342a12e937f
Kai Luedemann <kluedema@ualberta.ca>
2023-08-28 13:59:56 -0600

    Improved plots

--- atari_prediction/results_vis.py
+++ atari_prediction/results_vis.py
@@ -3,12 +3,15 @@ import tensorflow as tf
 import sys
 import os
 import matplotlib.pyplot as plt
+from scipy import stats
 
 
 def main(dir_path):
     epochs = 35
     avgs = []
+    sds = []
     preds = []
+    fig, axs = plt.subplots(5, 7, figsize=(19, 9), layout="constrained")
     for mode in ["test"]:
         for i in range(epochs):
         #for i in [32]:  
@@ -18,23 +21,31 @@ def main(dir_path):
             reg = np.load(reg_path)
             y_path = os.path.join(dir_path, f"{mode}.npy")
             y = np.load(y_path)
-            weights_path = os.path.join(dir_path, f"HL_{i}_w.npy")
+            weights_path = os.path.join(dir_path, f"Reg_{i}_w.npy")
             weights = np.load(weights_path, allow_pickle=True)
             avgs.append([np.mean(x) for x in weights[:-2]])
+            sds.append([np.std(x) for x in weights[:-2]])
             print(avgs[-1])
-            # a = [np.mean(np.abs(x)) for x in weights]
-            # print(a)
-            # preds.append(np.mean(reg[:300]))
-            # print(preds[-1] - np.mean(y[:300]))
-            # plt.plot(y)
-            # plt.plot(hl)
-            # plt.plot(reg)
-            # plt.title(f"{mode} {i}")
-            # plt.show()
-    avgs = np.stack(avgs)
-    for x in avgs.T:
-        plt.plot(x)
+            axs[i // 7, i % 7].plot(y)
+            axs[i // 7, i % 7].plot(hl)
+            axs[i // 7, i % 7].plot(reg)
+    n = np.asarray([np.size(x) for x in weights[:-2]])
+    plt.title(f"{mode} {i}")
+    plt.show()
+    avgs = np.stack(avgs).T
+    sds = np.stack(sds)
+    ses = (sds / np.sqrt(n)).T
+    alpha = 0.05
+    t_stats = stats.t.ppf(1 - alpha / 2, n - 1)
+    print(t_stats)
+    x = range(epochs)
+    for i in range(avgs.shape[0]):
+        plt.plot(avgs[i])
+        plt.fill_between(x, avgs[i] - t_stats[i] * ses[i], avgs[i] + t_stats[i] * ses[i], alpha=0.2)
     #plt.plot(preds)
+    ax = plt.gca()
+    ax.spines["top"].set_visible(False)
+    ax.spines["right"].set_visible(False)
     plt.show()
             
 
