16b0bd2338ef1140217f4d26ed10064c13a81b30
Kai Luedemann <kluedema@ualberta.ca>
2023-08-29 15:55:05 -0600

    Prep for full-scale Atari rerun

--- atari_prediction/shared_experiment.sh
+++ atari_prediction/shared_experiment.sh
@@ -1,21 +1,20 @@
 #!/bin/bash
 #SBATCH --job-name=Parallel
 #SBATCH --output=%x-%j.out
-#SBATCH --time=0-12:00:00
-#SBATCH --cpus-per-task=6
-#SBATCH --mem=32000M
-#SBATCH --gres=gpu:1
+#SBATCH --time=1-00:00:00
+#SBATCH --nodes=1
+#SBATCH --exclusive
+#SBATCH --cpus-per-task=24
+#SBATCH --mem=128000M
+#SBATCH --gpus-per-node=p100:4
 #SBATCH --mail-user=kluedema@ualberta.ca
 #SBATCH --mail-type=ALL
 
 GAMES_FILE=games.txt
 BASE_DIR=~/scratch
-GPUS=$SLURM_GPUS
 
 POLICY_DIR=$SLURM_TMPDIR/data/policies
 DATA=$BASE_DIR/data/policies.zip
-
-export OMP_NUM_THREADS=$SLURM_CPUS_PER_TASK
 
 module load python/3.10 scipy-stack cuda cudnn
 virtualenv --no-download $SLURM_TMPDIR/env
@@ -27,4 +26,4 @@ pip install AutoROM-0.6.1-py3-none-any.whl AutoROM.accept-rom-license-0.6.1.tar.
 mkdir $SLURM_TMPDIR/data
 unzip $DATA -d $SLURM_TMPDIR/data
 
-parallel -j 84% 'CUDA_VISIBLE_DEVICES=$((({%} - 1) % $GPUS)) source run_game.sh {} &> {}.out' < $GAMES_FILE
+parallel 'CUDA_VISIBLE_DEVICES=$((({%} - 1) % 4)) source run_game.sh {} &> {}.out' < $GAMES_FILE
