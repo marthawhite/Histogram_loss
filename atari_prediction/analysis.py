5bd58d32d815b9304c5e23a5925bfd885b125457
Kai Luedemann <kluedema@ualberta.ca>
2023-08-29 08:48:49 -0600

    Used Histogram bin code from experiments directory

--- atari_prediction/analysis.py
+++ atari_prediction/analysis.py
@@ -7,25 +7,7 @@ import os
 import json
 from scipy.stats import entropy
 import pandas as pd
-
-
-def get_bins(n_bins, pad_ratio, sig_ratio):
-    """Return the histogram bins given the HL parameters.
-    
-    Params:
-        n_bins - the number of bins to create (includes padding)
-        pad_ratio - the number of sigma of padding to use on each side 
-        sig_ratio - the ratio of sigma to bin width
-
-    Returns: 
-        borders - a Tensor of n_bins + 1 bin borders
-        sigma - the sigma to use for HL-Gaussian
-    """
-    bin_width = 1 / (n_bins - 2 * sig_ratio * pad_ratio)
-    pad_width = sig_ratio * pad_ratio * bin_width
-    borders = tf.linspace(-pad_width, 1 + pad_width, n_bins + 1)
-    sigma = bin_width * sig_ratio
-    return borders, sigma
+from experiment.bins import get_bins
 
 
 def get_json(filename):
