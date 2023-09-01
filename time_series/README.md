3aaaf340f3770670007f604d678492796027c21b
SamScholnickHughes <scholnic@ualberta.ca>
2023-09-01 13:14:23 -0600

    add running instructions in README

--- time_series/README.md
+++ time_series/README.md
@@ -16,3 +16,13 @@ The Electricity Transformer Temperature (ETT) datasets were collected by ([Zhou 
  - MLP
 
 The Linear, DLinear, and NLinear models are based on the [LTSF-Linear](https://github.com/cure-lab/LTSF-Linear) GitHub repository and corresponding paper ([Zeng *et al.* 2022]((https://arxiv.org/pdf/2205.13504.pdf))). The transformer model is from a [Keras code example](https://keras.io/examples/timeseries/timeseries_classification_transformer/) and is based on the following paper ([Vaswani *et al.* 2017]((https://arxiv.org/pdf/1706.03762.pdf))). The LSTM Encoder-Decoder model uses an LSTM layer with encoder and decoder as MLP blocks of fully-connected layers. We also included a version of the LSTM Encoder-Decoder model that makes autoregressive predictions by feeding the input data back into the model. The MLP model expands on the linear models by using a simple multi-layer perceptron with ReLU activations. We included two variants for predicting features independently or using all features to predict each one. 
+
+# Instruction
+1. Setup your Python environment using `requirements.txt`
+2. Copy `main.py` to the project (outer) directory
+3. (optional) Modify model hyperparameters or datasets to run on by modifying lines 53-71 of `main.py`. Discriptions of hyperparameters is available on lines 26-50 of `main.py`
+4. Run `main.py <base_model> <loss>` 
+    where `base_model` is one of: `transformer`, `LSTM`, `linear`, `independent_dense`, or `dependent_dense`. And `loss` is one of: `HL` or `L2`
+5. Collect training progress results in `{loss}_{dataset}_{base_model}.json`
+
+Note that you can replace `main.py` with `model_analysis.py` in the above procedure to get the training progress results as well as the test set targets and model prediction after the last training epoch, as `{dataset}_targets.npy` and `{dataset}_{base_model}_{loss}.npy` respectively.
--- time_series/main.py
+++ time_series/main.py
@@ -16,11 +16,39 @@ from experiment.bins import get_bins
 from time_series.datasets import get_time_series_dataset
 
 
-def main():
+def main(base_model, loss):
     """Run the time series experiment.
     
     Params:
-        data_path - path to the data file
+        base_model(str):  Name of the base model
+        loss(str): Name of loss, either HL or L2
+        
+    General Params:
+        datasets: Array of the names of the datasets to train and test the model on
+        seq_len: Number of previous timesteps to be given as input to the model (this is the same for all channels)
+        pred_len: Number of future timesteps the model will predict (this is the same for all channels)
+        epochs: Number of epochs to train for
+        sig_ratio: Sigma ratio of the discretized histogram transform
+        pad_ratio: Padding ratio of the discretized histogram transform
+        n_bins: Number of bins in the discretized histogram
+        chans: Number of variables in the dataset
+        test_ratio: Ratio of test data to train data
+        batch_size: Batch Size for the training
+        drop: Name of column(s) that contain data that should not be used as inputs (for multiple columns use a list of strings)
+        metrics: Metrics for evaluating train and test performance
+        lr: Learning rate
+        input_target_offset: Number of steps between the last input time step and the first target time step
+        
+    Model Specific Params:
+        Transformer:
+            head_size: Size of head for key and query
+            n_heads: Number of attention heads
+            features: Number of feature dimensions
+        LSTM:
+            layers: Number of layers in the encoder and decoder
+            width: Width of the layers in the encoder and decoder
+        
+        
     """
     datasets = ["ETTh1", "ETTh2", "ETTm1", "ETTm2"]
     pred_len = 336
@@ -30,21 +58,22 @@ def main():
     pad_ratio = 3.
     n_bins = 100
     chans = 7
-    # head_size = 512
-    # n_heads = 8
-    # features = 128
-    # layers = 2
-    # width = 512
+    head_size = 512
+    n_heads = 8
+    features = 128
+    layers = 2
+    width = 512
     test_ratio = 0.25
     batch_size = 32
     drop = "date"
     metrics = ["mse", "mae"]
     lr = 1e-3
+    input_target_offset = 0
 
     for dataset in datasets:
         keras.utils.set_random_seed(1)
         data_path = f"{dataset}.csv"
-        train, test, dmin, dmax = get_time_series_dataset(data_path, drop, seq_len, pred_len, pred_len, test_ratio, batch_size, chans)
+        train, test, dmin, dmax = get_time_series_dataset(data_path, drop, seq_len, pred_len, pred_len, test_ratio, batch_size, chans, input_target_offset)
 
         borders, sigma = get_bins(n_bins, pad_ratio, sig_ratio, dmin, dmax)
         borders = tf.expand_dims(borders, -1)
@@ -52,26 +81,33 @@ def main():
 
         shape = train.element_spec[0].shape[1:]
 
-        # #base = transformer(shape, head_size, n_heads, features)
-        # #base = linear(chans, seq_len)
-        # base = lstm_encdec(width, layers, 0.5, shape)
-
-        # hlg = HLGaussian(base, borders, sigma, out_shape=(chans, pred_len))    
-        # hlg.compile(keras.optimizers.Adam(lr), None, metrics)
-        # hist = hlg.fit(train, epochs=epochs, verbose=2, validation_data=test)
-        # with open(f"HL_lstm_{dataset}.json", "w") as file:
-        #     json.dump(hist.history, file)
-
-        #base = transformer(shape, head_size, n_heads, features)
-        base = linear(chans, seq_len)
-        #base = lstm_encdec(width, layers, 0.5, shape)
-
-        reg = Regression(base, out_shape=(pred_len,))    
-        reg.compile(keras.optimizers.Adam(lr), "mse", metrics)
-        hist = reg.fit(train, epochs=epochs, verbose=2, validation_data=test)
-        with open(f"Reg_linear_{dataset}.json", "w") as file:
-            json.dump(hist.history, file)
+        out_shape = (pred_len,)
+        if base_model == "transformer":
+            base = transformer(shape, head_size, n_heads, features)
+        elif base_model == "LSTM":
+            out_shape = (chans, pred_len)
+            base = lstm_encdec(width, layers, 0.5, shape)
+        elif base_model == "linear":
+            base = linear(chans, seq_len)
+        elif base_model == "independent_dense":
+            base = independent_dense(chans, seq_len)
+        else:
+            base = dependent_dense(chans, seq_len)
+            
+        if loss == "HL":
+            hlg = HLGaussian(base, borders, sigma, out_shape=out_shape)    
+            hlg.compile(keras.optimizers.Adam(lr), None, metrics)
+            hist = hlg.fit(train, epochs=epochs, verbose=2, validation_data=test)
+            with open(f"HL_{dataset}_{base_model}.json", "w") as file:
+                json.dump(hist.history, file)
+        else:
+            reg = Regression(base, out_shape=out_shape)    
+            reg.compile(keras.optimizers.Adam(lr), "mse", metrics)
+            hist = reg.fit(train, epochs=epochs, verbose=2, validation_data=test)
+            with open(f"L2_{dataset}_{base_model}.json", "w") as file:
+                json.dump(hist.history, file)
+        
 
 
 if __name__ == "__main__":
-    main()
+    main(sys.argv[1], sys.argv[2])
